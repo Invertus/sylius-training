2fc34fef6fac1de05670f2134773eff7254b0b08
Kamil Kokot <kamil@kokot.me>
2020-01-08 13:16:10 +0200

    Flat rate extra fee calculator specs

--- spec/ShippingCalculator/FlatRateExtraFeeCalculatorSpec.php
+++ spec/ShippingCalculator/FlatRateExtraFeeCalculatorSpec.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace spec\App\ShippingCalculator;
 
 use App\ShippingCalculator\FlatRateExtraFeeCalculator;
+use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Sylius\Component\Core\Exception\MissingChannelConfigurationException;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
 use Sylius\Component\Core\Model\ShipmentInterface;
 use Sylius\Component\Core\Model\ShippingMethodInterface;
+use Sylius\Component\Core\Model\TaxonInterface;
 use Sylius\Component\Shipping\Calculator\CalculatorInterface;
 
 final class FlatRateExtraFeeCalculatorSpec extends ObjectBehavior
@@ -34,6 +38,102 @@ final class FlatRateExtraFeeCalculatorSpec extends ObjectBehavior
         ChannelInterface $channel
     ): void {
         $shipment->getOrder()->willReturn($order);
+        $shipment->getShippables()->willReturn(new ArrayCollection());
+        $order->getChannel()->willReturn($channel);
+        $channel->getCode()->willReturn('WEB');
+
+        $this->calculate($shipment, ['WEB' => ['amount' => 1500]])->shouldReturn(1500);
+    }
+
+    function it_adds_extra_fee_if_there_are_jeans_in_the_products(
+        ShipmentInterface $shipment,
+        OrderInterface $order,
+        ChannelInterface $channel,
+        ProductVariantInterface $productVariant,
+        ProductInterface $product,
+        TaxonInterface $jeansTaxon,
+        TaxonInterface $otherTaxon
+    ): void
+    {
+        $shipment->getOrder()->willReturn($order);
+        $shipment->getShippables()->willReturn(new ArrayCollection([
+            $productVariant->getWrappedObject(),
+        ]));
+
+        $productVariant->getProduct()->willReturn($product);
+        $product->getTaxons()->willReturn(new ArrayCollection([
+            $otherTaxon->getWrappedObject(),
+            $jeansTaxon->getWrappedObject(),
+        ]));
+
+        $otherTaxon->getCode()->willReturn('other');
+        $jeansTaxon->getCode()->willReturn('jeans');
+
+        $order->getChannel()->willReturn($channel);
+        $channel->getCode()->willReturn('WEB');
+
+        $this->calculate($shipment, ['WEB' => ['amount' => 1500]])->shouldReturn(3500);
+    }
+
+    function it_adds_only_one_extra_fee_even_if_there_are_more_jeans_produts(
+        ShipmentInterface $shipment,
+        OrderInterface $order,
+        ChannelInterface $channel,
+        ProductVariantInterface $firstJeansProductVariant,
+        ProductInterface $firstJeansProduct,
+        ProductVariantInterface $secondJeansProductVariant,
+        ProductInterface $secondJeansProduct,
+        TaxonInterface $jeansTaxon,
+        TaxonInterface $otherTaxon
+    ): void {
+        $shipment->getOrder()->willReturn($order);
+        $shipment->getShippables()->willReturn(new ArrayCollection([
+            $firstJeansProductVariant->getWrappedObject(),
+            $secondJeansProductVariant->getWrappedObject(),
+        ]));
+
+        $firstJeansProductVariant->getProduct()->willReturn($firstJeansProduct);
+        $firstJeansProduct->getTaxons()->willReturn(new ArrayCollection([
+            $otherTaxon->getWrappedObject(),
+            $jeansTaxon->getWrappedObject(),
+        ]));
+
+        $secondJeansProductVariant->getProduct()->willReturn($secondJeansProduct);
+        $secondJeansProduct->getTaxons()->willReturn(new ArrayCollection([
+            $jeansTaxon->getWrappedObject(),
+            $otherTaxon->getWrappedObject(),
+        ]));
+
+        $otherTaxon->getCode()->willReturn('other');
+        $jeansTaxon->getCode()->willReturn('jeans');
+
+        $order->getChannel()->willReturn($channel);
+        $channel->getCode()->willReturn('WEB');
+
+        $this->calculate($shipment, ['WEB' => ['amount' => 1500]])->shouldReturn(3500);
+    }
+
+    function it_does_not_add_extra_fee_if_there_are_no_jeans_in_the_products(
+        ShipmentInterface $shipment,
+        OrderInterface $order,
+        ChannelInterface $channel,
+        ProductVariantInterface $productVariant,
+        ProductInterface $product,
+        TaxonInterface $otherTaxon
+    ): void
+    {
+        $shipment->getOrder()->willReturn($order);
+        $shipment->getShippables()->willReturn(new ArrayCollection([
+            $productVariant->getWrappedObject(),
+        ]));
+
+        $productVariant->getProduct()->willReturn($product);
+        $product->getTaxons()->willReturn(new ArrayCollection([
+            $otherTaxon->getWrappedObject(),
+        ]));
+
+        $otherTaxon->getCode()->willReturn('other');
+
         $order->getChannel()->willReturn($channel);
         $channel->getCode()->willReturn('WEB');
 
